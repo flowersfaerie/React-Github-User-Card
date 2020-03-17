d983d4eab463ed1c1d7b4f71c73563a284080cf1
Sarah Flowers <flowersfaerie@gmail.com>
2020-03-16 20:49:00 -0700

    Created the basic scaffolding for the project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    userImages: [],
+    userText: ""
+  };
+
+  componentDidMount() {
+    fetch("https://api.github.com/users/flowersfairie")
+      .then(response => response.json())
+      .then(users => {
+        console.log("sf: App.js: CDM: fetch: then2: users: ", users);
+        this.setState({userImages: users.message});
+      })
+      .catch(error => console.error(error));
+  }
+
+  handleChanges = e => {
+    this.setState({ ...this.state, userText: e.target.value});
+  };
+
+  // handleFetchUsers = e => {
+  //   e.preventDefault();
+  //   fetch()
+  // }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Sarah's Followers!</h1>
+        {/* <input
+          type="text"
+          value={this.state.userText}
+          onChange={this.handleChanges}
+        />
+        <button onClick={this.handleFetchUsers}>Fetch User</button> */}
+        <br />
+        {this.state.userImages.map(userImg => {
+          return <img alt="user" src={userImg} />
+        })}
+      </div>
+    )
+  }
+
 }
 
 export default App;
